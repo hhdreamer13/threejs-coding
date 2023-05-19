a30f46b8c79a8e708cdb34db227603742c9547d9
HhDreamer <hh.dreamer@gmail.com>
2023-05-20 01:10:46 +0200

    commit pixelate haunted house.

---
+++ src/Experience/Experience.js
@@ -0,0 +1,14 @@
+import Sizes from "../utils/Sizes";
+
+export default class Experience {
+  constructor() {
+    // Global access
+    window.experience = this;
+
+    // Options
+    this.canvas = canvas;
+
+    // Setup
+    this.sizes = new Sizes();
+  }
+}
